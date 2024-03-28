cc886e52b959fdcb887408a674110085ee1ad9bb
Pritchad25 <pricts29@gmail.com>
2024-03-29 00:34:42 +0200

    Parsing Headers and using POSTMan to test our API.

--- app/index.js
+++ app/index.js
@@ -22,11 +22,14 @@ var server = http.createServer(function(req, res){
     //Get the HTTP Method
     var method = req.method.toLowerCase();
 
+    //Get the headers as an object
+    var headers = req.headers;
+
     //Send the response
     res.end('Hello World, Its Time For An Expert!\n');
 
     //Log the request path
-    console.log('Request received on path: '+trimmedPath+ ' with method: '+method+' and with these query string parameters', queryStringObject);
+    console.log('Request received with these headers', headers);
 
 });
 
