7d128ed001158c6a3f588c05aa5435a07d702e06
Pritchad25 <pricts29@gmail.com>
2024-03-29 00:27:34 +0200

    Parsing HTTP methods

--- app/index.js
+++ app/index.js
@@ -16,12 +16,14 @@ var server = http.createServer(function(req, res){
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    
+    //Get the HTTP Method
+    var method = req.method.toLowerCase();
+
     //Send the response
     res.end('Hello World, Its Time For An Expert!\n');
 
     //Log the request path
-    console.log('Request received on path: '+trimmedPath);
+    console.log('Request received on path: '+trimmedPath+ ' with method: '+method);
 
 });
 
