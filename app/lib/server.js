ffd8e37b214c63fc8c426e6ede7ea4676882b104
Pritchad25 <pricts29@gmail.com>
2024-03-30 16:54:22 +0200

    Refactoring for a GUI.

--- app/lib/server.js
+++ app/lib/server.js
@@ -1,8 +1,8 @@
 /**
-* These are server related tasks
-* 
-* 
-*/
+ * These are server related tasks
+ * 
+ * 
+ */
 
 //Dependencies
 var http = require('http'); //requiring the http module and assigning it to the variable http
@@ -75,17 +75,30 @@ server.unifiedServer = function(req, res){
         };
 
         //Route the request to the handler specified in the router
-        chosenHandler(data, function(statusCode, payload){
+        chosenHandler(data, function(statusCode, payload, contentType){
+
+            //Determine the type of response (fallback to JSON)
+            contentType = typeof(contentType) == 'string' ? contentType : 'json'; 
             //Use the status code called back by the handler or default to 200
             statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            //Use the payload called back by the handler or default to an empty object
-            payload = typeof(payload) == 'object' ? payload : {};
-            //Convert the paylooad to a string
-            var payloadString = JSON.stringify(payload);
-            //Return the response or Send the response
+            
+
             res.setHeader('Content-Type', 'application/json');
+            
+            //Return the response or Send the response parts that are content-specific
+            var payloadString = '';
+            if(contentType == 'json'){
+                res.setHeader('Content-Type', 'application/json');
+                payload = typeof(payload) == 'object' ? payload : {};
+                payloadString = JSON.stringify(payload);
+            }
+            if(contentType == 'html'){
+                res.setHeader('Content-Type', 'text/html');
+                payloadString = typeof(payload) == 'string' ? payload : '';
+            }
+            //Return the response-parts that are common to all content-types
             res.writeHead(statusCode);
-            res.end(payloadString);   
+            res.end(payloadString);  
             console.log('Returning this response: ', statusCode, payloadString);
         });    
     });
@@ -93,22 +106,31 @@ server.unifiedServer = function(req, res){
 
 //Defining a Request Router
 server.router = {
+    '' : handlers.index,
+    'account/create' : handlers.accountCreate,
+    'account/edit' : handlers.accountEdit,
+    'account/deleted' : handlers.accountDeleted,
+    'session/create' : handlers.sessionCreate,
+    'session/deleted' : handlers.sessionDeleted,
+    'checks/all' : handlers.checkList,
+    'checks/create' : handlers.checksCreate,
+    'checks/edit' : handlers.checksEdit,
     'ping' : handlers.ping,
-    'users' : handlers.users,
-    'tokens' : handlers.tokens,
-    'checks' : handlers.checks 
+    'api/users' : handlers.users,
+    'api/tokens' : handlers.tokens,
+    'api/checks' : handlers.checks 
 };
 
 //Init script
 server.init = function(){
     //Start the HTTP server
     server.httpServer.listen(config.httpPort, function(){
-        console.log("The server is listening on port "+config.httpPort);
+        console.log('\x1b[35m%s\x1b[0m', "The server is listening on port "+config.httpPort);
     });
 
     //Start the HTTPS server
     server.httpsServer.listen(config.httpsPort, function(){
-        console.log("The server is listening on port "+config.httpsPort);
+        console.log('\x1b[36m%s\x1b[0m', "The server is listening on port "+config.httpsPort);
     });
 };
 //Export the module
