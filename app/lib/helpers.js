51ac9d1c3152bad77f1d3b44890c5d5ed01309a9
Pritchad25 <pricts29@gmail.com>
2024-03-30 15:48:53 +0200

    a library that integrates with Twilio and allows us to send sms'es.

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -5,6 +5,9 @@
 //Dependencies
 var crypto = require('crypto');
 var config = require('./config');
+var querystring = require('querystring');
+var https = require('https');
+const { Stats } = require('fs');
 //Container for all the helpers
 var helpers = {};
 
@@ -30,7 +33,6 @@ helpers.parseJsonToObject = function(str) {
     }
 };
 
-
 //Create a string of random alphanumeric characters, of a given length
 helpers.createRandomString = function(strLength){
     strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
@@ -54,5 +56,63 @@ helpers.createRandomString = function(strLength){
     }
 };
 
+//Send an SMS message via Twilio
+helpers.sendTwilioSms = function(phone, msg, callback){
+    //Validate parameters
+    phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
+    msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
+    if (phone && msg){
+        //Configure the request payload
+        var payload = {
+            'from' : config.twilio.fromPhone,
+            'To' : '+1'+phone,
+            'Body' : msg
+        };
+
+        //Stringify the payload
+        var stringPayload = querystring.stringify(payload);
+
+        //Configure the request details
+        var requestDetails = {
+            'protocol' : 'https:',
+            'hostname' : 'api.twilio.com',
+            'method' : 'POST',
+            'path' : '/2010-04-01/Accounts/'+config.twilio.accountSid+'/Messages.json',
+            'auth' : config.twilio.accountSid+':'+config.twilio.authToken,
+            'headers' : {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Content-Length' : Buffer.byteLength(stringPayload)
+            }
+        };
+
+        //Instantiate the request object
+        var req = https.request(requestDetails, function(res){
+            //Grab the statis of the sent request
+            var status = res.statusCode;
+            //Callback succesfully if the request went through
+            if (status == 200 || status == 201){
+                callback(false);
+            }
+            else{
+                callback('Status code returned was '+status);
+            }
+        });
+
+        //Bind to the error event so it doesnt get thrown
+        req.on('error', function(e){
+            callback(e);
+        });
+        
+        // Add the payload
+        req.write(stringPayload);
+
+        //End the request
+        req.end();
+    }
+    else{
+        callback('Given parameters were missing or invalid');
+    }
+
+};
 //Export the module
 module.exports = helpers;
