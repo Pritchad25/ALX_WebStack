c08a6bde2aaa162c75ad93b4b0b6dd960d8b99de
Pritchad25 <pricts29@gmail.com>
2024-03-30 14:59:24 +0200

    Tokens are going to be the authentication mechanism for this application.

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -1,8 +1,7 @@
 /**
-* Helpers for various tasks
-* 
-*/
-
+ * Helpers for various tasks
+ * 
+ */
 //Dependencies
 var crypto = require('crypto');
 var config = require('./config');
@@ -21,14 +20,39 @@ helpers.hash = function(str){
 };
 
 //Parse a JSON string to an object in all cases, without throwing
-helpers.parseJsonToObject = function(str){
-    try{
+helpers.parseJsonToObject = function(str) {
+    try {
         var obj = JSON.parse(str);
         return obj;
-    }
-    catch{e}{
+    } catch (err) {
+        console.error("An error occurred:", err);
         return {};
     }
 };
+
+
+//Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = function(strLength){
+    strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+    if(strLength){
+        //Define all the possible characters that could go into a string
+        var possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+        //Start the final string
+        var str = '';
+        for(i = 1; i < strLength; i++){
+            //Get a random character from the possibleCharacters string
+            var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+            //Append this character to the final string 
+            str += randomCharacter;
+        }
+        //Return the final string
+        return str;
+    }
+    else{
+        return false;
+    }
+};
+
 //Export the module
 module.exports = helpers;
