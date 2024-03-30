e701d295a50f6ac376d7a3927ebfd735594e4fc5
Pritchad25 <pricts29@gmail.com>
2024-03-30 19:23:05 +0200

    building up the logic of the account creation page.

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -8,6 +8,7 @@
 var _data = require('./data');
 var helpers = require('./helpers');
 var config  = require('./config');
+const { clearLine } = require('readline');
 
 //Defining the Handlers
 var handlers = {};
@@ -32,6 +33,41 @@ handlers.index = function(data, callback){
         };
         //Read in a template as a string
         helpers.getTemplate('index', templateData, function(err, str){
+            if(!err && str){
+                //Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function(err, str){
+                    if(!err && str){
+                        //Return that page as HTML
+                        callback(200, str, 'html');
+                    }
+                    else{
+                        callback(500, undefined, 'html');
+                    }
+                });
+            }
+            else{
+                callback(500, undefined, 'html');
+            }
+        });
+    }
+    else{
+        callback(405, undefined, 'html');
+    }
+};
+
+//Create Account
+handlers.accountCreate = function(data, callback){
+    //Reject any request that isnt a GET
+    if(data.method == 'get'){
+
+        //Prepare data for interpolation
+        var templateData = {
+            'head.title' : 'Create an Account',
+            'head.description' : 'Signup is easy and only takes a few seconds.',
+            'body.class' : 'accountCreate'
+        };
+        //Read in a template as a string
+        helpers.getTemplate('accountCreate', templateData, function(err, str){
             if(!err && str){
                 //Add the universal header and footer
                 helpers.addUniversalTemplates(str, templateData, function(err, str){
