bcd76496f5cf3154f2f3c87b36c6cf084a7afc7f
Pritchad25 <pricts29@gmail.com>
2024-03-30 08:01:05 +0200

    Adding HTTPS support to our application.

--- app/config.js
+++ app/config.js
@@ -1,28 +1,30 @@
-/*
- * Create and export configuration variables
- *
- */
+/**
+* Create and Export configuration variables
+* 
+*/
 
-// Container for all environments
+//Container for all the environments
 var environments = {};
 
-// Staging (default) environment
+//Staging (default) environment
 environments.staging = {
-  'port' : 3000,
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
   'envName' : 'staging'
 };
 
-// Production environment
+//Production environment
 environments.production = {
-  'port' : 5000,
+  'httpPort' : 5000,
+  'httpsPort' : 5001,
   'envName' : 'production'
 };
 
-// Determine which environment was passed as a command-line argument
+//Determine which environment was passed as a comman-line argument
 var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
-// Check that the current environment is one of the environments above, if not default to staging
+//Check that the current environment is one of the environments above, if not, default to staging
 var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
 
-// Export the module
+//Export the module
 module.exports = environmentToExport;
