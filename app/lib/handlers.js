a676f8301a07e46cd5266d0198618f21d00a4169
Pritchad25 <pricts29@gmail.com>
2024-03-31 10:22:42 +0200

    Creating the dashboard aka checks/all page.

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -263,6 +263,40 @@ handlers.checksCreate = function(data, callback){
     }
 };
 
+//Dashboard (View All Checks)
+handlers.checksList = function(data, callback){
+    //Reject any request that isnt a GET
+    if(data.method == 'get'){
+
+        //Prepare data for interpolation
+        var templateData = {
+            'head.title' : 'Dashboard',
+            'body.class' : 'checksList'
+        };
+        //Read in a template as a string
+        helpers.getTemplate('checksList', templateData, function(err, str){
+            if(!err && str){
+                //Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function(err, str){
+                    if(!err && str){
+                        //Return that page as HTML
+                        callback(200, str, 'html');
+                    }
+                    else{
+                        callback(500, undefined, 'html');
+                    }
+                });
+            }
+            else{
+                callback(500, undefined, 'html');
+            }
+        });
+    }
+    else{
+        callback(405, undefined, 'html');
+    }
+};
+
 //Favicon
 handlers.favicon = function(data, callback){
     //Reject any request that isnt a GET
